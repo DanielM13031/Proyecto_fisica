71ba056afd71c8ca073dafeaced599134c467084
DanielM <54962223+DanielM13031@users.noreply.github.com>
2024-11-09 12:20:34 -0500

    creacion de las clases presa y depredaro

--- proyecto.py
+++ proyecto.py
@@ -16,5 +16,29 @@ def lotka_volterra(y, t, alpha, beta, delta, gamma):   #Sistema de ecucuaiones d
 y0 =[40,9] # presas, depredadores 
 t = np.linspace(0,200,1000) 
 
+class presa:
+    def __init__(self, vel, pos):
+        self.vel = vel
+        self.pos = pos
+
+class depredador:
+    def __init__(self,vel, pos):
+        self.vel = vel
+        self.pos = pos
+
+v_aleatorias_p = [(np.random.uniform(1,10),np.random.uniform(1,10)) for _ in range(100)]
+v_aleatorias_d = [(np.random.uniform(1,10),np.random.uniform(1,10)) for _ in range(100)]
+
+pos_aleatorias_p = [(np.random.uniform(1,50),np.random.uniform(1,50)) for _ in range(100)]
+pos_aleatorias_d = [(np.random.uniform(1,50),np.random.uniform(1,50)) for _ in range(100)]
+
+presas = []
+depredadores = []
+
+for i, j in v_aleatorias_p, pos_aleatorias_p:
+    presas.append(presa(i, j))
+
+for i, j in v_aleatorias_d, pos_aleatorias_d:
+    depredadores.append(depredador(i, j))
 
 solution = odeint(lotka_volterra, y0, t, args=(alpha,beta,delta,gamma))
