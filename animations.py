e5c1af8babff197a03ce319e441acd4c2b18e5eb
QueenR1014 <juanreinareyes@hotmail.com>
2024-11-14 22:43:08 -0500

    esquizofreniaaaaaaaaa

--- animations.py
+++ animations.py
@@ -16,23 +16,27 @@ class LotkaVolterra(Scene):
         title = Text("Lotka-Volterra", font_size= 72, color = RED).shift(UP * 0.4)
         subtitle = Text("Presas y Depredadores").shift(DOWN * 0.4)
 
-        ax = always_redraw(lambda :
-            Axes(
+        ax = Axes(
                 x_range = [0,200,20], 
                 y_range= [0,max(max(presas_lotka),max(depredadores_lotka)),2],
                 x_length= 5,
                 y_length= 5
-                ))
+                )
         
         
         labels = ax.get_axis_labels(x_label = "Tiempo",y_label = "Poblaci\'{o}n")
 
-        depredadores_plot = ax.plot_line_graph(
-            x_values = t,
-            y_values = presas_lotka,
-            line_color = RED,
+        ax_group = VGroup(ax,labels)    
 
-        )
+        ax_group.shift(LEFT * 5)    
+        depredadores_curve = VMobject()
+        depredadores_curve.set_points_smoothly([ax.coords_to_point(x,y) for x,y in depredadores_points])
+        depredadores_curve.set_color(RED)
+
+        presas_curve = VMobject()
+        presas_curve.set_points_smoothly([ax.coords_to_point(x,y) for x,y in presas_points])
+        presas_curve.set_color(BLUE)
+
         self.play(Write(title),Write(subtitle))
         self.wait(3)
 
@@ -49,7 +53,8 @@ class LotkaVolterra(Scene):
         #self.play(FadeOut(labels))
         self.remove(title)
         self.add(ax)
-        self.play(Create(depredadores_plot))
+        self.play(Create(depredadores_curve), Create(presas_curve), run_time = 20)
+        
         #self.play(ax.animate.scale(0.5).move_to([-5,0,0]))
         self.wait(0.5)
         #self.play(ax.animate.move_to([-5,0,0]))
